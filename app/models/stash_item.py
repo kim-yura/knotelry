05aebbd76e4e25801ac62fd77dfc804951887e07
Yu Ra Kim <yurakim.designs@gmail.com>
2022-04-20 13:38:57 -0400

    stash_item model: tags, attributes, stored_in; fabric attributes

--- app/models/stash_item.py
+++ app/models/stash_item.py
@@ -35,7 +35,11 @@ class Stash_Item(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
     type = db.Column(db.String(50))
+
     status = db.Column(db.String(50))
+    tags = db.Column(db.Text)
+    attributes = db.Column(db.Text)
+    stored_in = db.Column(db.Text)
 
     acquired = db.Column(db.DateTime)
     acquired_from = db.Column(db.Text)
@@ -45,16 +49,16 @@ class Stash_Item(db.Model):
     # Broader attributes
     colors = db.Column(db.Text)
 
-    # Fiber and yarn attributes
+    # FIBER, YARN, FABRIC
     dyer_name = db.Column(db.Text)
     colorway_name = db.Column(db.Text)
     fiber_content = db.Column(db.String(200))
 
-    # Fiber attributes
+    # FIBER
     fiber_quantity = db.Column(db.Float)
     fiber_quantity_unit = db.Column(db.String(5))
 
-    # Yarn attributes
+    # YARN
     yarn_weight = db.Column(db.String(20))
     length_per_skein = db.Column(db.Float)
     length_unit = db.Column(db.String(5))
@@ -64,6 +68,18 @@ class Stash_Item(db.Model):
     length_stashed = db.Column(db.Float)
     weight_stashed = db.Column(db.Float)
     is_handspun = db.Column(db.Boolean)
+
+    # FABRIC
+    fabric_width = db.Column(db.Integer)
+    fabric_width_unit = db.Column(db.String(2))
+    fabric_weight = db.Column(db.Float)
+    fabric_weight_unit = db.Column(db.String(2))
+    fabric_weight_area_unit = db.Column(db.String(2))
+    fabric_pattern_repeat_width = db.Column(db.Float)
+    fabric_pattern_repeat_height = db.Column(db.Float)
+    fabric_pattern_repeat_unit = db.Column(db.String(2))
+    # length_stashed
+    # length_unit
 
     notes = db.Column(db.Text)
 
@@ -81,7 +97,11 @@ class Stash_Item(db.Model):
             'title': self.title,
             'description': self.description,
             'type': self.type,
+
             'status': self.status,
+            'tags': self.tags,
+            'attributes': self.attributes,
+            'stored_in': self.stored_in,
 
             'acquired': self.acquired,
             'acquired_from': self.acquired_from,
@@ -90,13 +110,16 @@ class Stash_Item(db.Model):
 
             'colors': self.colors,
 
+            # FIBER, YARN, FABRIC
             'dyer_name': self.dyer_name,
             'colorway_name': self.colorway_name,
             'fiber_content': self.fiber_content,
 
+            # FIBER
             'fiber_quantity': self.fiber_quantity,
             'fiber_quantity_unit': self.fiber_quantity_unit,
 
+            # YARN
             'yarn_weight': self.yarn_weight,
             'length_per_skein': self.length_per_skein,
             'length_unit': self.length_unit,
@@ -106,6 +129,18 @@ class Stash_Item(db.Model):
             'length_stashed': self.length_stashed,
             'weight_stashed': self.weight_stashed,
             'is_handspun': self.is_handspun,
+
+            # FABRIC
+            'fabric_width': self.fabric_width,
+            'fabric_width_unit': self.fabric_width_unit,
+            'fabric_weight': self.fabric_weight,
+            'fabric_weight_unit': self.fabric_weight_unit,
+            'fabric_weight_area_unit': self.fabric_weight_area_unit,
+            'fabric_pattern_repeat_width': self.fabric_pattern_repeat_width,
+            'fabric_pattern_repeat_height': self.fabric_pattern_repeat_height,
+            'fabric_pattern_repeat_unit': self.fabric_pattern_repeat_unit,
+            # length_stashed
+            # length_unit
 
             'notes': self.notes,
             'created_at': self.created_at,
@@ -123,7 +158,11 @@ class Stash_Item(db.Model):
             'title': self.title,
             'description': self.description,
             'type': self.type,
+
             'status': self.status,
+            'tags': self.tags,
+            'attributes': self.attributes,
+            'stored_in': self.stored_in,
 
             'acquired': self.acquired,
             'acquiredFrom': self.acquired_from,
@@ -132,13 +171,16 @@ class Stash_Item(db.Model):
 
             'colors': self.colors,
 
+            # FIBER, YARN, FABRIC
             'dyerName': self.dyer_name,
             'colorwayName': self.colorway_name,
             'fiberContent': self.fiber_content,
 
+            # FIBER
             'fiberQuantity': self.fiber_quantity,
             'fiberQuantityUnit': self.fiber_quantity_unit,
 
+            # YARN
             'yarnWeight': self.yarn_weight,
             'lengthPerSkein': self.length_per_skein,
             'lengthUnit': self.length_unit,
@@ -148,6 +190,18 @@ class Stash_Item(db.Model):
             'lengthStashed': self.length_stashed,
             'weightStashed': self.weight_stashed,
             'isHandspun': self.is_handspun,
+
+            # FABRIC
+            'fabricWidth': self.fabric_width,
+            'fabricWidthUnit': self.fabric_width_unit,
+            'fabricWeight': self.fabric_weight,
+            'fabricWeightUnit': self.fabric_weight_unit,
+            'fabricWeightAreaUnit': self.fabric_weight_area_unit,
+            'fabricPatternRepeatWidth': self.fabric_pattern_repeat_width,
+            'fabricPatternRepeatHeight': self.fabric_pattern_repeat_height,
+            'fabricPatternRepeatUnit': self.fabric_pattern_repeat_unit,
+            # lengthStashed
+            # lengthUnit
 
             'notes': self.notes,
             'createdAt': self.created_at,
