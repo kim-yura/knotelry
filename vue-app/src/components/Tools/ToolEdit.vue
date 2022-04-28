6e95fec73d41324e3210680a523b9ce18923ef22
Yu Ra Kim <yurakim.designs@gmail.com>
2022-04-28 10:59:04 -0400

    Refactored formatDate function

--- vue-app/src/components/Tools/ToolEdit.vue
+++ vue-app/src/components/Tools/ToolEdit.vue
@@ -276,18 +276,7 @@ const loadTool = async (id) => {
 
 const formatDate = (date) => {
     const d = new Date(date);
-    let month = "" + (d.getMonth() + 1);
-    let day = "" + d.getDate();
-    let year = d.getFullYear();
-
-    if (month.length < 2) {
-        month = "0" + month;
-    }
-    if (day.length < 2) {
-        day = "0" + day;
-    }
-
-    return [year, month, day].join("-");
+    return d.toISOString().split("T")[0];
 };
 </script>
 
