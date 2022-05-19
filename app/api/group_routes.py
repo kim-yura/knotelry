7a1366f521bdba121558122270c3021b6deb732e
Yu Ra Kim <yurakim.designs@gmail.com>
2022-05-19 14:47:40 -0400

    Group routes: get group's members

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Blueprint, jsonify, make_response, request
-from app.models import db, Group, Group_Membership
+from app.models import db, Group, Group_Membership, User
 
 group_routes = Blueprint('group', __name__)
 
@@ -21,5 +21,13 @@ def get_users_groups(id):
     users_groups = []
     for membership in users_memberships:
         users_groups.append(Group.query.get(membership.group_id))
-
     return {'usersGroups': [group.to_JSON() for group in users_groups]}
+
+@group_routes.route('/<int:id>/members')
+def get_groups_members(id):
+    group_memberships = Group_Membership.query.all()
+    group_members = []
+    for membership in group_memberships:
+        if membership.group_id == id:
+            group_members.append(User.query.get(membership.user_id))
+    return {'groupMembers': [member.to_JSON() for member in group_members]}
