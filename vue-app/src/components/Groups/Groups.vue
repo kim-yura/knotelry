7e221853b28e478a9461a8cd617bdf89631d3692
Yu Ra Kim <yurakim.designs@gmail.com>
2022-05-23 12:30:20 -0400

    Groups left panel

--- vue-app/src/components/Groups/Groups.vue
+++ vue-app/src/components/Groups/Groups.vue
@@ -5,6 +5,26 @@
                 You are in {{ usersGroups?.length }} group
             </h3>
             <h3 v-else>You are in {{ usersGroups?.length }} groups</h3>
+            <div
+                class="group-preview"
+                v-for="group in usersGroups"
+                :key="group.id"
+            >
+                <img
+                    v-if="group.bannerImageURL"
+                    class="preview-image"
+                    :src="group.bannerImageURL"
+                    alt="Group banner image"
+                />
+                <img
+                    v-else
+                    class="preview-image"
+                    src="https://knotelry.s3.amazonaws.com/image-placeholder.png"
+                    alt="No banner image"
+                />
+                <p>{{ group.title }}</p>
+
+            </div>
         </div>
     </div>
 </template>
@@ -70,13 +90,40 @@ const loadUsersGroups = async (userId) => {
 <style scoped>
 .groups-page {
     display: grid;
-    grid-template-columns: 1fr 3fr;
+    grid-template-columns: 1fr 2fr;
     column-gap: 4%;
+    padding-bottom: 16px;
     border: 1px solid yellow;
 }
 
 .groups-page-left {
     min-height: calc(100vh - 68px);
     border: 1px solid yellow;
+}
+
+.group-preview {
+    display: grid;
+    grid-template-columns: 60px 1fr;
+    column-gap: 20px;
+    padding-left: 12px;
+    padding-right: 12px;
+    padding-bottom: 12px;
+    height: 60px;
+}
+
+.preview-image {
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+    border: 1px solid var(--color-shadow);
+    border-radius: 4px;
+}
+
+.group-preview > p {
+    text-align: left;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    align-self: center;
+    font-size: 14px;
 }
 </style>
