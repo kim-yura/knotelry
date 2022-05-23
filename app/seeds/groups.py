87664f953948e0e475f64d12966d3a12abea0afc
Yu Ra Kim <yurakim.designs@gmail.com>
2022-05-23 12:08:32 -0400

    Updated groups seed file

--- app/seeds/groups.py
+++ app/seeds/groups.py
@@ -41,6 +41,76 @@ def seed_groups():
         user_id=3,
     )
 
+    group3 = Group(
+        id=3,
+        title="Bloomington B's",
+        description="A home for Bloomington crafters",
+        country="US",
+        owner_id=1,
+    )
+    group3_member1 = Group_Membership(
+        group_id=3,
+        user_id=1,
+    )
+    group3_member2 = Group_Membership(
+        group_id=3,
+        user_id=2,
+    )
+    group3_member3 = Group_Membership(
+        group_id=3,
+        user_id=3,
+    )
+
+    group4 = Group(
+        id=4,
+        title="Test Group 1",
+        owner_id=1,
+    )
+    group4_member1 = Group_Membership(
+        group_id=4,
+        user_id=1,
+    )
+
+    group5 = Group(
+        id=5,
+        title="Test Group 2",
+        owner_id=1,
+    )
+    group5_member1 = Group_Membership(
+        group_id=5,
+        user_id=1,
+    )
+
+    group6 = Group(
+        id=6,
+        title="Test Group 3",
+        owner_id=1,
+    )
+    group6_member1 = Group_Membership(
+        group_id=6,
+        user_id=1,
+    )
+
+    group7 = Group(
+        id=7,
+        title="Test Group 4",
+        owner_id=1,
+    )
+    group7_member1 = Group_Membership(
+        group_id=7,
+        user_id=1,
+    )
+
+    group8 = Group(
+        id=8,
+        title="Test Group 5",
+        owner_id=1,
+    )
+    group8_member1 = Group_Membership(
+        group_id=8,
+        user_id=1,
+    )
+
     db.session.add(group1)
     db.session.add(group1_member1)
     db.session.add(group1_member2)
@@ -49,6 +119,26 @@ def seed_groups():
     db.session.add(group2)
     db.session.add(group2_member1)
     db.session.add(group2_member2)
+
+    db.session.add(group3)
+    db.session.add(group3_member1)
+    db.session.add(group3_member2)
+    db.session.add(group3_member3)
+
+    db.session.add(group4)
+    db.session.add(group4_member1)
+
+    db.session.add(group5)
+    db.session.add(group5_member1)
+
+    db.session.add(group6)
+    db.session.add(group6_member1)
+
+    db.session.add(group7)
+    db.session.add(group7_member1)
+
+    db.session.add(group8)
+    db.session.add(group8_member1)
 
     db.session.commit()
 
